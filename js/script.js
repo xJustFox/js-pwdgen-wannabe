b588eaa192b172102201a11720a6ee5e0701a625
xJustFox <dodydaversa@gmail.com>
2023-11-17 16:14:06 +0100

    fixed console log

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 // Creata variabile "name"
 let name = prompt("Inserisci il tuo nome");
+console.log(name)
 
 // Creata variabile "surname"
 let surname = prompt("Inserisci il tuo cognome");
+console.log(surname)
 
 // Creata variabile "color"
 let color = prompt("Inserisci il tuo colore preferito");
+console.log(color)
 
 // Creata la variabile "number"
 const number = 23;
+console.log(number)
 
 // Concatenate le variabili 
-document.getElementById("password").innerHTML = `${document.getElementById("password").innerHTML}${name}${surname}${color}${number}`
-console.log(document.getElementById("password").innerHTML1)
+document.getElementById("password").innerHTML = `${document.getElementById("password").innerHTML}${name}${surname}${color}${number}`;
+console.log(document.getElementById("password").innerHTML);
