ee7d10869f187b54ccf2dc94af98b17ef101f8ea
xJustFox <dodydaversa@gmail.com>
2023-11-17 14:14:18 +0100

    Created the "number" variable

--- js/script.js
+++ js/script.js
@@ -6,3 +6,6 @@ let surname = prompt("Inserisci il tuo cognome");
 
 // Creata variabile "color"
 let color = prompt("Inserisci il tuo colore preferito");
+
+// Creata la variabile "number"
+let number = 23;
